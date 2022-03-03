b0788929ac43a467d5ee6080adba775d93842b5b
unknown <mcairney@calibrateconsulting.com>
2022-03-03 14:44:01 +0000

    Integrated API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import * as d3 from 'd3';
-import LineChart from './LineChart';
-import SideBar from './SideBar';
-import Radial from './RadialChart';
+import { useEffect, useState } from 'react';
+import LineChart from './components/LineChart';
+import SideBar from './components/SideBar';
+import Radial from './components/RadialChart';
 import styled from 'styled-components';
-import CardDetail from './CardDetail';
+import CardDetail from './components/CardDetail';
+import { fetchData } from './util/API';
 
 const RowContainer1 = styled.div`
   display: flex;
@@ -37,26 +39,150 @@ const RowCard = styled.div`
 `;
 
 export default function App() {
-  return (
-    <RowContainer1>
-      <SideBar />
-      <ColContainer>
-        <RowCard>
-          <CardDetail />
-          <CardDetail />
-          <CardDetail />
-          <CardDetail />
-          <CardDetail />
-          <CardDetail />
-          <CardDetail />
-        </RowCard>
-        <LineChart />
-        <RowContainer2>
-          <Radial percent={22} measure="Humidity" />
-          <Radial percent={88} measure="Cloudcover" />
-          <Radial percent={55} />
-        </RowContainer2>
-      </ColContainer>
-    </RowContainer1>
+  const [weatherData, setWeatherData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const getWeatherData = async () => {
+    const response = await fetchData();
+    setWeatherData(response);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    getWeatherData();
+    setLoading(false);
+  }, []);
+
+  const daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  var days = weatherData?.daily?.time?.map(
+    (time) => daysOfWeek[new Date(time).getDay()],
   );
+
+  // CLOUDCOVER RADIAL
+  const currCloudCover =
+    weatherData?.hourly?.cloudcover
+      ?.slice(0, 24)
+      .reduce((prev, curr) => prev + curr) / 24;
+
+  // HUMIDITY RADIAL
+  const currHumidity =
+    weatherData?.hourly?.relativehumidity_2m
+      ?.slice(0, 24)
+      .reduce((prev, curr) => prev + curr) / 24;
+
+  // GET APPROPRIATE INFO BASED ON API WEATHERCODE
+  const getWeatherCode = (code) => {
+    switch (code) {
+      default:
+        var weather = 'Not found';
+        var emoji = null;
+        var symbol = null;
+      case 0:
+        weather = 'Clear sky';
+        emoji = '&#128526;';
+        break;
+      case 1:
+        weather = 'Mainly clear';
+        emoji = '&#128578;';
+        symbol = '&#x2600;';
+        break;
+      case 2:
+        weather = 'Partly cloudy';
+        emoji = '&#128527;';
+        symbol = '&#x1F324;';
+        break;
+      case 3:
+        weather = 'Overcast';
+        emoji = '&#128528;';
+        symbol = '&#x2601;';
+        break;
+      case 61:
+        weather = 'Slight rain';
+        emoji = '&#128531;';
+        symbol = '&#x1F327;';
+        break;
+      case 63:
+        weather = 'Moderate rain';
+        break;
+      case 65:
+        weather = 'Heavy rain';
+        break;
+    }
+    return { weather, emoji, symbol };
+  };
+
+  if (loading) {
+    return <p>loading</p>;
+  } else {
+    return (
+      <RowContainer1>
+        <SideBar
+          data={weatherData}
+          forecastLabel={
+            getWeatherCode(weatherData?.current_weather?.weathercode)
+              .weather
+          }
+          emoji={
+            getWeatherCode(weatherData?.current_weather?.weathercode)
+              .emoji
+          }
+          symbol={
+            getWeatherCode(weatherData?.current_weather?.weathercode)
+              .symbol
+          }
+        />
+        <ColContainer>
+          <RowCard>
+            {days?.map((day, index) => (
+              <CardDetail
+                weekday={day}
+                forecastLabel={
+                  getWeatherCode(
+                    weatherData?.daily?.weathercode[index],
+                  ).weather
+                }
+                emoji={
+                  getWeatherCode(
+                    weatherData?.daily?.weathercode[index],
+                  ).emoji
+                }
+                symbol={
+                  getWeatherCode(
+                    weatherData?.daily?.weathercode[index],
+                  ).symbol
+                }
+                highTemp={
+                  weatherData?.daily?.temperature_2m_max[index]
+                }
+                lowTemp={
+                  weatherData?.daily?.temperature_2m_min[index]
+                }
+              />
+            ))}
+          </RowCard>
+          <LineChart data={weatherData} />
+          <RowContainer2>
+            <Radial
+              percent={Math.round(currHumidity)}
+              measure='Humidity'
+            />
+            <Radial
+              percent={Math.round(currCloudCover)}
+              measure='Cloudcover'
+            />
+            <Radial percent={55} />
+          </RowContainer2>
+        </ColContainer>
+      </RowContainer1>
+    );
+  }
 }
---
+++ src/components/LineChart.jsx
@@ -0,0 +1,128 @@
+import * as d3 from 'd3';
+import { useRef, useEffect } from 'react';
+import useWindowSize from '../util/useWindowSize';
+
+function LineChart({ data }) {
+
+  const currHour = new Date().getHours();
+  const currDate = new Date().getDate();
+  const temps = data?.hourly?.temperature_2m?.slice(currHour);
+  const times = data?.hourly?.time?.slice(currHour);
+  const forecast = temps?.map((item, index) => [
+    item,
+    new Date(times[index]),
+  ]);
+
+  const svgRef = useRef();
+  const size = useWindowSize();
+  const margin = { top: 0, bottom: 50, left: 20, right: 110 };
+  const height = size.height;
+  const width = size.width;
+
+  const labels = forecast?.map((val) => <text key={Math.random()}/>);
+  // Will be called initially and on every data change
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+
+    // Define scales for x and y
+    const xScale = d3
+      .scaleTime()
+      .domain([
+        new Date(2022, 2, currDate, currHour, 0),
+        new Date(2022, 2, currDate + 1, currHour, 0),
+      ])
+      .range([0, width]);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([-10, 20])
+      .range([height, 0]);
+
+    // Define axis for x and y
+    const xAxis = d3.axisBottom(xScale).tickSize(0).tickPadding(10);
+    // AREA
+    if (forecast) {
+      d3.select(svgRef.current)
+        .select('.areaChart')
+        .datum(forecast)
+        .attr('transform', `translate(${margin.left}, ${margin.top})`)
+        .attr(
+          'd',
+          d3
+            .area()
+            .curve(d3.curveCardinal)
+            .x((d) => xScale(d[1]))
+            .y0(height)
+            .y1((d) => yScale(Math.round(d[0]))),
+        )
+        .attr('font-family', 'sans-serif')
+        .style('stroke', 'none')
+        .style('stroke-width', '2')
+        .style('fill', 'url(#temperature-gradient)');
+    }
+
+    // Linear gradient
+    const color = d3.scaleSequential(
+      yScale.domain(),
+      d3.interpolateTurbo,
+    );
+    d3.select(svgRef.current)
+      .select('.linGrad')
+      .attr('id', 'temperature-gradient')
+      .attr('gradientUnits', 'userSpaceOnUse')
+      .attr('x1', 0)
+      .attr('y1', height + margin.bottom)
+      .attr('x2', 0)
+      .attr('y2', margin.top - height / 4)
+      .selectAll('stop')
+      .data(d3.ticks(0, 1, 10))
+      .join('stop')
+      .attr('offset', (d) => d)
+      .attr('stop-color', color.interpolator());
+
+    if (forecast) {
+      d3.select(svgRef.current)
+        .select('.labels')
+        .selectAll('text')
+        .data(forecast)
+        .text((d) => `${Math.round(d[0])}°`)
+        .attr('text-anchor', 'middle')
+        .attr('x', (d) => xScale(d[1]))
+        .attr('y', (d) => yScale(d[0]))
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '13px')
+        .attr('opacity', '0.65')
+        .attr(
+          'transform',
+          `translate(${margin.left}, ${margin.top - 12})`,
+        )
+        .style('cursor', 'default');
+    }
+    // Append x axis
+    svg
+      .select('.xAxis')
+      .call(xAxis)
+      .attr(
+        'transform',
+        `translate(${margin.left}, ${height + margin.top})`,
+      )
+      .select('path')
+      .attr('opacity', '0');
+  }, [forecast]);
+  return (
+    <div className='div_temp'>
+      <svg
+        height={height + margin.bottom}
+        width={width + margin.right}
+        ref={svgRef}
+      >
+        <linearGradient className='linGrad' />
+        <g className='xAxis' />
+        <path className='areaChart' />
+        <g className='labels'>{labels}</g>
+      </svg>
+    </div>
+  );
+}
+
+export default LineChart;
